cb494afba76c6166722d1042990a597551b55401
BioCSK <katd6@naver.com>
2021-03-01 23:54:46 +0900

    commit

--- frontend_vanila/myapp/js/index.js
+++ frontend_vanila/myapp/js/index.js
@@ -1,6 +1,17 @@
 const mainTxt = document.querySelector(".aboutMeTxt");
 const topNav = document.querySelector(".topNavi-component");
-console.log(mainTxt.innerHTML);
+const myProfileImg = document.getElementById("myProfilePic");
+
+
+myProfileImg.addEventListener("mouseenter",function(){
+    myProfileImg.style.opacity = "1";
+})
+
+myProfileImg.addEventListener("mouseleave",function(){
+    myProfileImg.style.opacity = "0.8";
+})
+
+
 
 let mainTxtsep='';
 for (let i = 0; i < mainTxt.innerHTML.length ; i ++){
@@ -33,3 +44,23 @@ window.addEventListener("scroll",(e)=>{
         topNav.children[0].classList.remove("on");
     }
 })
+
+
+google.charts.load('current', {'packages':['timeline']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var container = document.getElementById('educationTimeline');
+  var chart = new google.visualization.Timeline(container);
+  var dataTable = new google.visualization.DataTable();
+
+  dataTable.addColumn({ type: 'string', id: 'education' });
+  dataTable.addColumn({ type: 'date', id: 'Start' });
+  dataTable.addColumn({ type: 'date', id: 'End' });
+  dataTable.addRows([
+    [ '가천대학교 생명과학과 ', new Date(2015, 03, 01), new Date(2019, 08, 22) ],
+    [ '서울대학교 ' ,new Date(2019,08,20), new Date(2020,05,01)],
+    [ '서강대학교 소프트웨어' ,new Date(2021,03,01),new Date(2021,03,01)]
+  ])
+
+  chart.draw(dataTable);
+}
